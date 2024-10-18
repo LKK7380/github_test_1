7ff66c787f2e239af60be146b0263153e36b3f24
unknown <dlrlrhkd@naver.com>
2024-10-18 20:33:06 +0900

    add some files

---
+++ variable.ex/src/variable/ex/Application.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class Application {
+    public int stringAdd(String str) {
+        if(str == null || str.isEmpty()) {
+            return 0;
+        }
+        return 0; // 구현 빈 공간이거나 null인경우에 0을 반환하는것
+    }
+}
