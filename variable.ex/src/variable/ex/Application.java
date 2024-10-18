c5ad5f9e53935e3ee4907928a6f1ee6924e8ae23
unknown <dlrlrhkd@naver.com>
2024-10-18 21:14:07 +0900

    variable.ex/src/variable/ex/Application.java 파일 코드 추가

--- variable.ex/src/variable/ex/Application.java
+++ variable.ex/src/variable/ex/Application.java
@@ -5,6 +5,14 @@ public class Application {
         if(str == null || str.isEmpty()) {
             return 0;
         }
+        if(str.contains(",")) {
+            String[] plusValues = str.split(",");
+            int sum = 0;
+            for (String plusValue : plusValues) {
+                sum += Integer.parseInt(plusValue);
+            } // 문자열안에 ,가 있을때 정상적으로 문자열을 더하는가?
+            return sum;
+        }
         return 0; // 구현 빈 공간이거나 null인경우에 0을 반환하는것
     }
 }
